4f7a6461ac9c8babdad08d49424fbd403d385948
alstj0606 <alstj0606@nate.com>
2024-02-29 13:23:02 +0900

    update new file

--- venv/MP.py
+++ venv/MP.py
@@ -1,5 +1,4 @@
 class Member:
-
     def __init__(self, name, password, email):
         self.name = name
         self.email = email
@@ -9,9 +8,25 @@ class Member:
         print(f"이름: {self.name}, email(ID): {self.email}")
 
 class Post:
-
     def __init__(self, title, content, author):
         self.title = title
         self.content = content
         self.author = author
 
+# 회원 정보 리스트
+members = {}
+
+# 회원 추가
+def add_member():
+    name = input("이름을 입력하세요: ")
+    password = input("비밀번호를 입력하세요: ")
+    email = input("이메일을 입력하세요: ")
+    if email in members:
+        member = Member(name, password, email)
+        members[email] = member
+        print("회원이 추가되었습니다.")
+
+
+add_member()
+add_member()
+find_member()
